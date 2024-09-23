e10a2ab31f5f7cf30df4ba6e3e48b849e3dd04dc
GabrielePugliese <163171821+GabrielePugliese@users.noreply.github.com>
2024-09-23 11:54:10 +0200

    scrittura in js

---
+++ script/js/script.js
@@ -0,0 +1,60 @@
+conteggio=0
+
+let ans1 = prompt("1. Chi ha scritto la Divina Commedia?")
+ans1=ans1.toLowerCase()
+ans1=ans1.trim()
+if(ans2 === dante){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans2 = prompt("2. Capoluogo della Puglia")
+ans2=ans2.toLowerCase()
+ans2=ans2.trim()
+if(ans2 === bari){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans3 = prompt("3. Cosa c'è dopo il due?")
+ans3=ans3.toLowerCase()
+ans3=ans3.trim()
+if(ans3 === tre){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans4 = prompt("4. Chi ha vinto il mondiale 202")
+ans4=ans4.toLowerCase()
+ans4=ans4.trim()
+if(ans4 === argentina){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans5 = prompt("5. Il migliore amico dell'uomo")
+ans5=ans5.toLowerCase()
+ans5=ans5.trim()
+if(ans5 === cane){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans6 = prompt("6. Chi ha vinto Sanremo 2024?")
+ans6=ans6.toLowerCase()
+ans6=ans6.trim()
+if(ans6 === angelina){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+let ans7 = prompt("7. La limonata è fatti di?")
+ans7=ans7.toLowerCase()
+ans7=ans7.trim()
+if(ans7 === angelina){
+    conteggio++
+    alert("punteggio: ",conteggio)
+}
+
+
+
